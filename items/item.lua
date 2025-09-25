9cf96e1e06d7da676a3d5f0ed18bd4f90aee453b
etherware-novice <73374039+etherware-novice@users.noreply.github.com>
2025-09-24 20:38:51 -0500

    oops

--- items/item.lua
+++ items/item.lua
@@ -19,7 +19,7 @@ SMODS.Consumable {
     atlas = "item",
     pos = { x = 1, y = 0 },
 
-    config = { max_highlighted=1, mod_conv = "m_nflame_rules" },
+    config = { max_highlighted=2, mod_conv = "m_nflame_rules" },
     loc_vars = function(self, info_queue, card)
         info_queue[#info_queue + 1] = G.P_CENTERS[card.ability.mod_conv]
 	return { vars = { card.ability.max_highlighted, localize { type = 'name_text', set = 'Enhanced', key = card.ability.mod_conv } } }
